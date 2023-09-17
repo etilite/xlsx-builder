66a25fce3cd904fe102136206a4606686eede528
Alexander <good.cannibal@gmail.com>
2023-09-17 22:10:47 +0300

    feat: add graceful shutdown for http server through context.Done()

--- delivery/http/server.go
+++ delivery/http/server.go
@@ -1,8 +1,11 @@
 package http
 
 import (
+	"context"
+	"errors"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Server struct {
@@ -18,7 +21,22 @@ func NewServer(addr string, h http.Handler) *Server {
 	}
 }
 
-func (s *Server) Start() {
-	log.Printf("Starting web server on port %s", s.http.Addr)
-	log.Fatal(s.http.ListenAndServe())
+func (s *Server) Run(ctx context.Context) {
+	go func() {
+		<-ctx.Done()
+		shutdownCtx, done := context.WithTimeout(context.Background(), 5*time.Second)
+		defer done()
+
+		log.Printf("http-server: shutting down")
+		if err := s.http.Shutdown(shutdownCtx); err != nil {
+			log.Printf("http-server: shutdown error: %v", err)
+		}
+	}()
+
+	log.Printf("http-server: starting web server on port %s", s.http.Addr)
+	if err := s.http.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		log.Printf("http-server: failed to serve: %v", err)
+	}
+
+	log.Printf("http-server: stopped gracefully")
 }
--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
+	"os/signal"
+	"syscall"
+
 	"github.com/etilite/xlsx-builder/delivery/http"
 	"github.com/etilite/xlsx-builder/internal/config"
 )
 
 func main() {
+	ctx, done := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer done()
+
+	realMain(ctx)
+}
+
+func realMain(ctx context.Context) {
 	cfg := config.Read()
 	mux := http.NewRouter()
 	srv := http.NewServer(cfg.HTTPAddr, mux)
 
-	srv.Start()
+	srv.Run(ctx)
 }
